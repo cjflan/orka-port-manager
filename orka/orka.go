143fabbab4c0ffddb2e25cbf9081aab42bcfa237
cjflan <89868659+cjflan@users.noreply.github.com>
2024-04-26 17:00:07 -0500

    this probably works but i havent tested it

--- orka/orka.go
+++ orka/orka.go
@@ -1,13 +1,14 @@
 package orka
 
 import (
-    "bytes"
-    "encoding/json"
-    "flag"
-    "fmt"
-    "io"
-    "net/http"
-    "strings"
+	"bytes"
+	"encoding/json"
+	"flag"
+	"fmt"
+	"io"
+	"net/http"
+	"strings"
+	"time"
 )
 
 const API_URL = "http://10.221.188.20" 
@@ -35,7 +36,10 @@ func main() {
 
     port := orka.getPort()
 
-    orka.DeployVM(port)
+    vm := orka.DeployVM(port)
+    reservedPorts := orka.ReservedPorts(vm.VMID)
+
+    fmt.Printf("reservedPorts: %v\n", reservedPorts)
 }
 
 type TokenResponse struct {
@@ -145,40 +149,135 @@ func (o *OrkaClient) getPort() int {
 type VMDeployRequest struct {
     OrkaVMName string `json:"orka_vm_name"`
     OrkaNodeName string `json:"orka_node_name"` 
-    ReservedPorts []string `json:"reserved_ports"`
+    ReservedPorts string `json:"reserved_ports"`
+}
+type StatusResponse struct {
+    Message string `json:"message"`
+    Help    struct {
+    } `json:"help"`
+    Errors                  []any `json:"errors"`
+    VirtualMachineResources []struct {
+        VirtualMachineName string `json:"virtual_machine_name"`
+        VMDeploymentStatus string `json:"vm_deployment_status"`
+        Status             []struct {
+            Owner                 string `json:"owner"`
+            VirtualMachineName    string `json:"virtual_machine_name"`
+            VirtualMachineID      string `json:"virtual_machine_id"`
+            NodeLocation          string `json:"node_location"`
+            NodeStatus            string `json:"node_status"`
+            VirtualMachineIP      string `json:"virtual_machine_ip"`
+            VncPort               string `json:"vnc_port"`
+            ScreenSharingPort     string `json:"screen_sharing_port"`
+            SSHPort               string `json:"ssh_port"`
+            CPU                   int    `json:"cpu"`
+            Vcpu                  int    `json:"vcpu"`
+            Gpu                   string `json:"gpu"`
+            RAM                   string `json:"RAM"`
+            BaseImage             string `json:"base_image"`
+            Image                 string `json:"image"`
+            ConfigurationTemplate string `json:"configuration_template"`
+            VMStatus              string `json:"vm_status"`
+            IoBoost               bool   `json:"io_boost"`
+            NetBoost              bool   `json:"net_boost"`
+            UseSavedState         bool   `json:"use_saved_state"`
+            ReservedPorts         []ReservedPorts `json:"reserved_ports"`
+            CreationTimestamp time.Time `json:"creationTimestamp"`
+            Tag               string    `json:"tag"`
+            TagRequired       bool      `json:"tag_required"`
+        } `json:"status"`
+    } `json:"virtual_machine_resources"`
 }
 
+type ReservedPorts struct {
+    HostPort  int    `json:"host_port"`
+    GuestPort int    `json:"guest_port"`
+    Protocol  string `json:"protocol"`
+}
 
-func (o *OrkaClient) DeployVM(port int) {
+type DeployResponse struct {
+    Message string `json:"message"`
+    Help    struct {
+        StartVirtualMachine            string `json:"start_virtual_machine"`
+        StopVirtualMachine             string `json:"stop_virtual_machine"`
+        ResumeVirtualMachine           string `json:"resume_virtual_machine"`
+        SuspendVirtualMachine          string `json:"suspend_virtual_machine"`
+        DataForVirtualMachineExecTasks struct {
+            OrkaVMName string `json:"orka_vm_name"`
+        } `json:"data_for_virtual_machine_exec_tasks"`
+        VirtualMachineVnc string `json:"virtual_machine_vnc"`
+    } `json:"help"`
+    Errors          []any  `json:"errors"`
+    RAM             string `json:"ram"`
+    Vcpu            string `json:"vcpu"`
+    HostCPU         string `json:"host_cpu"`
+    IP              string `json:"ip"`
+    SSHPort         string `json:"ssh_port"`
+    ScreenSharePort string `json:"screen_share_port"`
+    VMID            string `json:"vm_id"`
+    PortWarnings    []any  `json:"port_warnings"`
+    IoBoost         bool   `json:"io_boost"`
+    NetBoost        bool   `json:"net_boost"`
+    UseSavedState   bool   `json:"use_saved_state"`
+    GpuPassthrough  bool   `json:"gpu_passthrough"`
+    VncPort         string `json:"vnc_port"`
+}
+
+func (o *OrkaClient) DeployVM(port int) DeployResponse {
+    client := o.Client
     url := fmt.Sprint(API_URL, "/resources/vm/deploy")
 
-    payload := strings.NewReader(`{
-        "orka_vm_name": "myorkavm"
-        }`)
+    ports := fmt.Sprint("8080:", port)
 
-    client := &http.Client {
+    vmDeploy := VMDeployRequest{
+        OrkaVMName: "port-test",
+        ReservedPorts: ports,
     }
-    req, err := http.NewRequest(http.MethodPost, url, payload)
+
+    reqBytes, _ := json.Marshal(vmDeploy)
+    req, err := http.NewRequest(
+        http.MethodPost,
+        url, bytes.NewReader(reqBytes),
+        )
 
     if err != nil {
-        fmt.Println(err)
-        return
+        panic(err)
     }
     req.Header.Add("Content-Type", "application/json")
-    req.Header.Add("Authorization", "Bearer myToken")
+    req.Header.Add("Authorization", o.token)
 
     res, err := client.Do(req)
     if err != nil {
-        fmt.Println(err)
-        return
+        panic(err)
     }
     defer res.Body.Close()
 
+    var response DeployResponse
     body, err := io.ReadAll(res.Body)
-    if err != nil {
-        fmt.Println(err)
-        return
-    }
-    fmt.Println(string(body))
+    json.Unmarshal(body, &response)
 
+    return response
+    
 }
+
+func (o *OrkaClient) ReservedPorts(vmid string) []ReservedPorts {
+    url := fmt.Sprint(API_URL, "/resources/vm/status/myorkavm")
+    client := o.Client
+    req, err := http.NewRequest(http.MethodGet, url, nil)
+
+    if err != nil {
+        panic(err)
+    }
+    req.Header.Add("Authorization", o.token)
+
+    res, err := client.Do(req)
+    if err != nil {
+        panic(err)
+    }
+    defer res.Body.Close()
+
+    var response StatusResponse
+    body, err := io.ReadAll(res.Body)
+    json.Unmarshal(body, &response)
+
+    return response.VirtualMachineResources[0].Status[0].ReservedPorts
+    }
